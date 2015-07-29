8edd90cd0503f439363603e5b4f11faecbc9c5f1
nuriozt <nuriozt@gmail.com>
2015-07-29 18:37:27 +0300

    wfAracKiralama
    araç sorgulamayı hallettim

---
+++ wfAracKiralama/wfAracKiralama/cGenel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfAracKiralama
+{
+    class cGenel
+    {
+        public static string connStr = "Data Source=ACER; Initial Catalog=AraçKiralama; uid=sa; pwd=12345678";
+
+        public static string MusteriTipi ="";
+    }
+}
---
+++ wfAracKiralama/wfAracKiralama/frmAracSorgulama.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wfAracKiralama
+{
+    public partial class frmAracSorgulama : Form
+    {
+        public frmAracSorgulama()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAracSorgulama_Load(object sender, EventArgs e)
+        {
+            List<cArac> list = new List<cArac>();
+            cArac a = new cArac();
+
+            txtMarkayaGore.Text = "";
+            cbKiraDurumu.SelectedIndex = 0;
+            cbSanzimanTipi.SelectedIndex = 0;
+            cbYakitTuru.SelectedIndex = 0;
+
+
+            dgvAraclar.DataSource = a.AraclariGetir();
+        }
+
+        private void btnGetir_Click(object sender, EventArgs e)
+        {
+            cArac a = new cArac();
+
+            if (cbSanzimanTipi.SelectedItem.ToString() == "Hepsi" && cbYakitTuru.SelectedItem.ToString() == "Hepsi" && cbKiraDurumu.SelectedItem.ToString() == "Hepsi" && txtMarkayaGore.Text == null)
+            {
+                a.AraclariGetir();
+            }
+            else
+            {
+                bool kiradurumu = true;
+
+                if (cbKiraDurumu.SelectedItem.ToString() == "Uygun")
+                {
+                    kiradurumu = true;
+                }
+                else if (cbKiraDurumu.SelectedItem.ToString() == "Uygun Değil")
+                {
+                    kiradurumu = false;
+                }
+               
+                dgvAraclar.DataSource = a.AraclariGetirByDetayli(cbSanzimanTipi.SelectedItem.ToString(), cbYakitTuru.SelectedItem.ToString(), kiradurumu,cbKiraDurumu.SelectedItem.ToString(),txtMarkayaGore.Text);
+            }
+        }
+
+        private void txtMarkayaGore_TextChanged(object sender, EventArgs e)
+        {
+            cArac a = new cArac();
+
+            if (cbSanzimanTipi.SelectedItem.ToString() == "Hepsi" && cbYakitTuru.SelectedItem.ToString() == "Hepsi" && cbKiraDurumu.SelectedItem.ToString() == "Hepsi" && txtMarkayaGore.Text == null)
+            {
+                a.AraclariGetir();
+            }
+            else
+            {
+                bool kiradurumu = true;
+
+                if (cbKiraDurumu.SelectedItem.ToString() == "Uygun")
+                {
+                    kiradurumu = true;
+                }
+                else if (cbKiraDurumu.SelectedItem.ToString() == "Uygun Değil")
+                {
+                    kiradurumu = false;
+                }
+
+                dgvAraclar.DataSource = a.AraclariGetirByDetayli(cbSanzimanTipi.SelectedItem.ToString(), cbYakitTuru.SelectedItem.ToString(), kiradurumu, cbKiraDurumu.SelectedItem.ToString(),txtMarkayaGore.Text);
+            }
+        }
+    }
+}
