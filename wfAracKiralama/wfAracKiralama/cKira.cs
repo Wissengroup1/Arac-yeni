9354f88e59da43b5543f4daa07a8247e36dc1530
nuriozt <nuriozt@gmail.com>
2015-08-05 15:32:59 +0300

    05.08.2015 15:32

---
+++ wfAracKiralama/wfAracKiralama/cKira.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wfAracKiralama
+{
+    class cKira
+    {
+        private int _kiraID;
+        private int _musteriNo;
+        private int _firmaNo;
+        private int _aracNo;
+        private DateTime _kirayaGidisTarihi;
+        private DateTime _kiradanDonusTarihi;
+        private int _tutar;
+        private int _kiraDetayID;
+
+        #region Properties 
+        public int KiraID
+        {
+            get { return _kiraID; }
+            set { _kiraID = value; }
+        }
+        public int MusteriNo
+        {
+            get { return _musteriNo; }
+            set { _musteriNo = value; }
+        }
+
+
+        public int FirmaNo
+        {
+            get { return _firmaNo; }
+            set { _firmaNo = value; }
+        }
+
+
+        public int AracNo
+        {
+            get { return _aracNo; }
+            set { _aracNo = value; }
+        }
+
+
+        public DateTime KirayaGidisTarihi
+        {
+            get { return _kirayaGidisTarihi; }
+            set { _kirayaGidisTarihi = value; }
+        }
+
+
+        public DateTime KiradanDonusTarihi
+        {
+            get { return _kiradanDonusTarihi; }
+            set { _kiradanDonusTarihi = value; }
+        }
+
+
+        public int Tutar
+        {
+            get { return _tutar; }
+            set { _tutar = value; }
+        }       
+        public int KiraDetayID
+        {
+            get { return _kiraDetayID; }
+            set { _kiraDetayID = value; }
+        }
+        #endregion
+
+        public bool BireyselKirala(cKira bk)
+        {
+            int a = 0;
+            SqlConnection conn = new SqlConnection(cGenel.connStr);
+            SqlCommand comm3 = new SqlCommand("select KiraDetayID from KiraDetay", conn);
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            SqlDataReader dr;
+            try
+            {
+                dr = comm3.ExecuteReader();
+                while (dr.Read())
+                {
+                    a = Convert.ToInt32(dr[0]);
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+            }
+            conn.Close();
+            a++;
+
+            SqlCommand comm1 = new SqlCommand("Insert into KiraDetay (ToplamTutar,KiraDetayID) values(@ToplamTutar,@KiraDetayID)", conn);
+            comm1.Parameters.Add("@ToplamTutar", SqlDbType.Int).Value = bk._tutar;
+            comm1.Parameters.Add("@KiraDetayID", SqlDbType.Int).Value = a;
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            comm1.ExecuteNonQuery();
+            conn.Close();
+            SqlCommand comm2 = new SqlCommand("select KiraDetayID from KiraDetay", conn);
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            SqlDataReader dr1;
+            try
+            {
+                dr1 = comm2.ExecuteReader();
+                while (dr1.Read())
+                {
+                    bk._kiraDetayID = Convert.ToInt32(dr1[0]);
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+            }
+            conn.Close();
+
+            SqlCommand comm = new SqlCommand("Insert into Kiralama (MusteriNo,AracNo,KirayaGidisTarihi,KiradanDonusTarihi,Tutar,KiraDetayID) values(@MusteriNo,@AracNo,@KirayaGidisTarihi,@KiradanDonusTarihi,@Tutar,@KiraDetayID)", conn);
+            comm.Parameters.Add("@MusteriNo", SqlDbType.Int).Value = bk._musteriNo;
+            comm.Parameters.Add("@AracNo", SqlDbType.Int).Value = bk._aracNo;
+            comm.Parameters.Add("@KirayaGidisTarihi", SqlDbType.DateTime).Value = bk._kirayaGidisTarihi;
+            comm.Parameters.Add("@KiradanDonusTarihi", SqlDbType.DateTime).Value = bk._kiradanDonusTarihi;
+            comm.Parameters.Add("@Tutar", SqlDbType.Int).Value = bk._tutar;
+            comm.Parameters.Add("@KiraDetayID", SqlDbType.Int).Value = bk._kiraDetayID;          
+
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            bool Sonuc = Convert.ToBoolean(comm.ExecuteNonQuery());
+            conn.Close();
+            return Sonuc;
+        }
+
+        public List<cKira> KiralananGetirBireysel()
+        {
+            List<cKira> list = new List<cKira>();
+            SqlConnection conn = new SqlConnection(cGenel.connStr);
+            SqlCommand comm = new SqlCommand("Select * from Kiralama where Silindi=0 and MusteriNo not like 'NULL'", conn);
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            SqlDataReader dr;
+            try
+            {              
+                dr = comm.ExecuteReader();
+                while (dr.Read())
+                {
+                    cKira a = new cKira();
+                    a._kiraID = Convert.ToInt32(dr["KiraID"]);                   
+                    a._musteriNo = Convert.ToInt32(dr["MusteriNo"]);
+                    a._aracNo = Convert.ToInt32(dr["AracNo"]);
+                    a._kirayaGidisTarihi = Convert.ToDateTime(dr["KirayaGidisTarihi"]);
+                    a._kiradanDonusTarihi = Convert.ToDateTime(dr["KiradanDonusTarihi"]);
+                    a._tutar = Convert.ToInt32(dr["Tutar"]);
+                    a._kiraDetayID = Convert.ToInt32(dr["KiraDetayID"]);
+                    list.Add(a);
+                }
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+            }
+            return list;
+        }
+
+        public List<cKira> KiralananGetirKurumsal()
+        {
+            List<cKira> list = new List<cKira>();
+            SqlConnection conn = new SqlConnection(cGenel.connStr);
+            SqlCommand comm = new SqlCommand("Select * from Kiralama where Silindi=0 and FirmaNo not like 'NULL'", conn);
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            SqlDataReader dr;
+            try
+            {
+                dr = comm.ExecuteReader();
+                while (dr.Read())
+                {
+                    cKira a = new cKira();
+                    a._kiraID = Convert.ToInt32(dr["KiraID"]);
+                    a._firmaNo = Convert.ToInt32(dr["FirmaNo"]);
+                    a._aracNo = Convert.ToInt32(dr["AracNo"]);
+                    a._kirayaGidisTarihi = Convert.ToDateTime(dr["KirayaGidisTarihi"]);
+                    a._kiradanDonusTarihi = Convert.ToDateTime(dr["KiradanDonusTarihi"]);
+                    a._tutar = Convert.ToInt32(dr["Tutar"]);
+                    a._kiraDetayID = Convert.ToInt32(dr["KiraDetayID"]);
+                    list.Add(a);
+                }
+                for (int i = 0; i < cGenel.AracEkle; i++)
+                {
+                    
+                }
+                
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+            }
+            return list;
+        }
+
+        public void KurumsalKirala(cKira kk)
+        {
+       
+            SqlConnection conn = new SqlConnection(cGenel.connStr);
+            SqlCommand comm1 = new SqlCommand("Insert into KiraDetay (ToplamTutar,KiraDetayID) values(@ToplamTutar,@KiraDetayID)", conn);
+
+            comm1.Parameters.Add("@ToplamTutar", SqlDbType.Int).Value = kk._tutar;
+            comm1.Parameters.Add("@KiraDetayID", SqlDbType.Int).Value = cGenel.KiraDetayKurumsal;
+
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            comm1.ExecuteNonQuery();
+            conn.Close();
+
+            SqlCommand comm2 = new SqlCommand();
+
+            comm2.Connection = conn;
+            comm2.CommandText = "Insert into Kiralama (FirmaNo,AracNo,KirayaGidisTarihi,KiradanDonusTarihi,Tutar,KiraDetayID) values (@FirmaNo,@AracNo,@KirayaGidisTarihi,@KiradanDonusTarihi,@Tutar,@KiraDetayID)";
+
+            comm2.Parameters.Add("@FirmaNo", SqlDbType.Int).Value = kk._firmaNo;
+            comm2.Parameters.Add("@AracNo", SqlDbType.Int).Value = kk._aracNo;
+            comm2.Parameters.Add("@KirayaGidisTarihi", SqlDbType.DateTime).Value = kk._kirayaGidisTarihi;
+            comm2.Parameters.Add("@KiradanDonusTarihi", SqlDbType.DateTime).Value = kk._kiradanDonusTarihi;
+            comm2.Parameters.Add("@Tutar", SqlDbType.Int).Value = kk._tutar;
+            comm2.Parameters.Add("@KiraDetayID", SqlDbType.Int).Value = cGenel.KiraDetayKurumsal;
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            comm2.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public int KiraDetayIDGetir()
+        {
+            int a = 0;
+            SqlConnection conn = new SqlConnection(cGenel.connStr);
+            SqlCommand comm = new SqlCommand("select KiraDetayID from KiraDetay", conn);
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            SqlDataReader dr;
+            try
+            {
+                dr = comm.ExecuteReader();
+                while (dr.Read())
+                {
+                    a = Convert.ToInt32(dr[0]);
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+            }
+            conn.Close();
+            int ID = a + 1;
+            return ID;
+        }
+    }
+}
---
+++ wfAracKiralama/wfAracKiralama/frmAracEkle.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wfAracKiralama
+{
+    public partial class frmAracEkle : Form
+    {
+        public frmAracEkle()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAracEkle_Load(object sender, EventArgs e)
+        {
+            this.Top = 0;
+            this.Left = 0;
+            btnKaydet.Visible = false;
+            btnSil.Visible = false;
+            btnDegistir.Visible = false;
+            rbKayitEkle.Checked = true;
+            btnAracGetir.Visible = false;
+          
+        }
+
+        private void rbKayitEkle_CheckedChanged(object sender, EventArgs e)
+        {
+            Temizle();
+            if (rbKayitEkle.Checked == true)
+            {
+                btnKaydet.Visible = true;
+                btnSil.Visible = false;
+                btnDegistir.Visible = false;
+                btnAracGetir.Visible = false;
+                ReadOnlyFalse();
+            }
+            else if (rbKayitSil.Checked == true)
+            {
+                btnSil.Visible = true;
+                btnKaydet.Visible = false;
+                btnDegistir.Visible = false;
+                btnAracGetir.Visible = true;
+                ReadOnlyTrue();
+
+            }
+
+            else
+            {
+                btnDegistir.Visible = true;
+                btnKaydet.Visible = false;
+                btnSil.Visible = false;
+                btnAracGetir.Visible = true;
+                ReadOnlyFalse();
+            }
+        }
+        private void rbKayitDegistirme_CheckedChanged(object sender, EventArgs e)
+        {
+            Temizle();
+            if (rbKayitEkle.Checked == true)
+            {
+                btnKaydet.Visible = true;
+                btnSil.Visible = false;
+                btnDegistir.Visible = false;
+                btnAracGetir.Visible = false;
+                ReadOnlyFalse();
+            }
+            else if (rbKayitSil.Checked == true)
+            {
+                btnSil.Visible = true;
+                btnKaydet.Visible = false;
+                btnDegistir.Visible = false;
+                btnAracGetir.Visible = true;
+                ReadOnlyTrue();
+            }
+
+            else
+            {
+                btnDegistir.Visible = true;
+                btnKaydet.Visible = false;
+                btnSil.Visible = false;
+                btnAracGetir.Visible = true;
+                ReadOnlyFalse();
+            }
+        }
+        private void rbKayitSil_CheckedChanged(object sender, EventArgs e)
+        {
+            Temizle();
+            if (rbKayitEkle.Checked == true)
+            {
+                btnKaydet.Visible = true;
+                btnSil.Visible = false;
+                btnDegistir.Visible = false;
+                btnAracGetir.Visible = false;
+                ReadOnlyFalse();
+            }
+            else if (rbKayitSil.Checked == true)
+            {
+                btnSil.Visible = true;
+                btnKaydet.Visible = false;
+                btnDegistir.Visible = false;
+                btnAracGetir.Visible = true;
+                ReadOnlyTrue();
+
+            }
+
+            else
+            {
+                btnDegistir.Visible = true;
+                btnKaydet.Visible = false;
+                btnSil.Visible = false;
+                btnAracGetir.Visible = true;
+                ReadOnlyFalse();
+            }
+        }
+
+        private void Temizle()
+        {
+            txtID.Clear();
+            txtMarka.Clear();
+            txtModel.Clear();
+            txtPlaka.Clear();
+            txtYil.Clear();
+            txtLokasyon.Clear();
+            txtKm.Clear();
+            txtKasaTipi.Clear();
+            txtRenk.Clear();
+            txtKoltukSayisi.Clear();
+            txtYakitCinsi.Clear();
+            txtSHacmi.Clear();
+            txtCekis.Clear();
+            txtSasiNo.Clear();
+            txtSanzimanTuru.Clear();
+            txtGunlukUcret.Clear();
+            cbSanzimanTuru.SelectedIndex = -1;
+            cbYakitCinsi.SelectedIndex = -1;
+        }
+        private void ReadOnlyTrue()
+        {
+            txtMarka.ReadOnly = true;
+            txtModel.ReadOnly = true;
+            txtYil.ReadOnly = true;
+            txtPlaka.ReadOnly = true;
+            txtLokasyon.ReadOnly = true;
+            txtKm.ReadOnly = true;
+            txtKasaTipi.ReadOnly = true;
+            txtRenk.ReadOnly = true;
+            txtKoltukSayisi.ReadOnly = true;
+            txtSHacmi.ReadOnly = true;
+            txtCekis.ReadOnly = true;
+            txtSasiNo.ReadOnly = true;
+            txtGunlukUcret.ReadOnly = true;
+
+        }
+        private void ReadOnlyFalse()
+        {
+            txtMarka.ReadOnly = false;
+            txtModel.ReadOnly = false;
+            txtYil.ReadOnly = false;
+            txtPlaka.ReadOnly = false;
+            txtLokasyon.ReadOnly = false;
+            txtKm.ReadOnly = false;
+            txtKasaTipi.ReadOnly = false;
+            txtRenk.ReadOnly = false;
+            txtKoltukSayisi.ReadOnly = false;
+            txtSHacmi.ReadOnly = false;
+            txtCekis.ReadOnly = false;
+            txtSasiNo.ReadOnly = false;
+            txtGunlukUcret.ReadOnly = false;
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            if (txtMarka.Text.Trim() != "" && txtModel.Text.Trim() != "" && txtPlaka.Text.Trim() != ""&& txtYil.Text.Trim() != ""&& txtKm.Text.Trim() != ""&& txtKasaTipi.Text.Trim() != ""&& txtRenk.Text.Trim() != ""&& txtKoltukSayisi.Text.Trim() != ""&& txtYakitCinsi.Text.Trim() != ""&& txtSHacmi.Text.Trim() != ""&& txtCekis.Text.Trim() != ""&& txtSasiNo.Text.Trim() != ""&& txtSanzimanTuru.Text.Trim() != ""&& txtGunlukUcret.Text.Trim() != "")
+            {
+
+                cArac a = new cArac();
+                if (a.AracKontrol(txtSasiNo.Text))
+                {
+                    MessageBox.Show("Önceden sisteme girilmiş!", "Zaten Kayıtlı Araç");
+                    Temizle();
+                    txtMarka.Focus();
+                }
+                else
+                {
+                    a.Marka = txtMarka.Text;
+                    a.Model = txtModel.Text;
+                    a.Plaka = txtPlaka.Text;
+                    a.Yil = Convert.ToInt32(txtYil.Text);
+                    a.Lokasyon = txtLokasyon.Text;
+                    a.Km = Convert.ToInt32(txtKm.Text);
+                    a.KasaTipi = txtKasaTipi.Text;
+                    a.Renk = txtRenk.Text;
+                    a.KoltukSayisi = txtKoltukSayisi.Text;
+                    a.YakitCinsi = txtYakitCinsi.Text;
+                    a.SilindirHacmi = Convert.ToInt32(txtSHacmi.Text);
+                    a.Cekis = txtCekis.Text;
+                    a.SasiNo = txtSasiNo.Text;
+                    a.SanzimanTuru = txtSanzimanTuru.Text;
+                    a.GunlukUcret = Convert.ToInt32(txtGunlukUcret.Text);
+
+
+                    bool Sonuc = a.AracEkle(a);
+                    if (Sonuc)
+                    {
+                        MessageBox.Show("Araç Bilgileri eklendi.");
+                        Temizle();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Araç eklenemedi!!");
+                    }
+                }
+            }
+            else { MessageBox.Show("Gerekli alanları doldurmadınız!!"); }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text != "" || txtID.Text == "0")
+            {
+                if (MessageBox.Show("Silmek İstiyor musunuz?", "Eminmisiniz?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    cArac a = new cArac();
+                    if (a.AracSil(txtID.Text))
+                    {
+                        MessageBox.Show("Araç Silindi.");
+                        Temizle();
+                        ReadOnlyFalse();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bir Araç Seçmelisiniz!");
+            }
+        }
+
+        private void btnDegistir_Click(object sender, EventArgs e)
+        {
+            if (txtMarka.Text != "" && txtModel.Text != "" && txtPlaka.Text != "" && txtYil.Text != "" && txtKm.Text != "" && txtKasaTipi.Text != "" && txtRenk.Text != "" && txtKoltukSayisi.Text != "" && txtKoltukSayisi.Text != "" && txtYakitCinsi.Text != "" && txtSHacmi.Text != "" && txtCekis.Text != "" && txtSasiNo.Text != "" && txtSanzimanTuru.Text != "" && txtGunlukUcret.Text != "")
+            {
+                cArac a = new cArac();
+                a.AracID = Convert.ToInt32(txtID.Text);
+                a.Marka = txtMarka.Text;
+                a.Model = txtModel.Text;
+                a.Plaka = txtPlaka.Text;
+                a.Lokasyon = txtLokasyon.Text;
+                a.Yil = Convert.ToInt32(txtYil.Text);
+                a.Km = Convert.ToInt32(txtKm.Text);
+                a.KasaTipi = txtKasaTipi.Text;
+                a.Renk = txtRenk.Text;
+                a.KoltukSayisi = txtKoltukSayisi.Text;
+                a.YakitCinsi = txtYakitCinsi.Text;
+                a.SilindirHacmi = Convert.ToInt32(txtSHacmi.Text);
+                a.Cekis = txtCekis.Text;
+                a.SasiNo = txtSasiNo.Text;
+                a.SanzimanTuru = txtSanzimanTuru.Text;
+                a.GunlukUcret = Convert.ToInt32(txtGunlukUcret.Text);
+                if (a.AracGuncelle(a))
+                {
+                    MessageBox.Show("Değişiklikler Yapıldı.");
+                    Temizle();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Eksik Bilgiler Bulunmaktadır!");
+            }
+        }   
+
+        private void cbYakitCinsi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtYakitCinsi.Text = cbYakitCinsi.Text;
+        }
+
+        private void cbSanzimanTuru_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtSanzimanTuru.Text = cbSanzimanTuru.Text;
+        }
+
+        private void btnAracGetir_Click(object sender, EventArgs e)
+        {
+            frmAracSorgulama frm = new frmAracSorgulama();
+            frm.ShowDialog();
+
+            cArac arc = new cArac();
+            cArac a = new cArac();
+
+            txtID.Text = cGenel.AracID.ToString();
+
+            arc.AraclariGetirByAracID(cGenel.AracID, a);
+
+            txtMarka.Text = a.Marka;
+            txtModel.Text = a.Model;
+            txtPlaka.Text = a.Plaka;
+            txtYil.Text = a.Yil.ToString();
+            txtLokasyon.Text = a.Lokasyon;
+            txtKm.Text = a.Km.ToString();
+            txtKasaTipi.Text = a.KasaTipi;
+            txtRenk.Text = a.Renk;
+            txtKoltukSayisi.Text = a.KoltukSayisi;
+            txtYakitCinsi.Text = a.YakitCinsi;
+            txtSHacmi.Text = a.SilindirHacmi.ToString();
+            txtCekis.Text = a.Cekis;
+            txtSasiNo.Text = a.SasiNo;
+            txtSanzimanTuru.Text = a.SanzimanTuru;
+            txtGunlukUcret.Text = a.GunlukUcret.ToString();
+            
+        }
+    }
+}
